1657c91f0a7afc5f67f2f0bbe73fed0c720f48f4
Benjamin Clos <benjamin.clos@gmail.com>
2015-10-11 13:32:13 -0400

    remove old travis build shell script

--- travis-deps.sh
+++
@@ -1,5 +0,0 @@
-sudo add-apt-repository ppa:rethinkdb/ppa -y
-sudo apt-get update -y
-sudo apt-get install rethinkdb -y
-sudo mkdir -p /etc/rethinkdb/instances.d
-sudo -- sh -c "echo 'bind=all' > /etc/rethinkdb/instances.d/default.conf"
